d35a5d7e2f75387dda077b3cb1f8ea02e71bf17a
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-10-20 12:01:34 +0530

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,26 @@
+function bubbleSort(arr) {
+  var len = arr.length;
+  var swapped;
+  
+  do {
+    swapped = false;
+    
+    for (var i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // Swap the elements
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  
+  return arr;
+}
+
+// Example usage:
+var unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+var sortedArray = bubbleSort(unsortedArray);
+console.log("Sorted array: " + sortedArray);
